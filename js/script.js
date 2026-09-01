3afc45f85d5312754dd1a34b7bac1c161aa9ed2f
aniramjee01 <ani.ramjee@gmail.com>
2026-09-01 12:57:37 -0400

    Light mode dark mode part one
    added light and dark mode start up

--- js/script.js
+++ js/script.js
@@ -1,4 +1,55 @@
-let today = new Date();
+// ==========================================
+// DARK / LIGHT MODE
+// ==========================================
 
-document.getElementById("date").textContent =
-    "Today's date is " + today.toLocaleDateString();
+function toggleTheme() {
+
+    document.body.classList.toggle("light-mode");
+
+    const button = document.getElementById("themeToggle");
+
+
+    if (document.body.classList.contains("light-mode")) {
+
+        button.textContent = "🌙 Dark Mode";
+
+        localStorage.setItem("theme", "light");
+
+    } else {
+
+        button.textContent = "☀️ Light Mode";
+
+        localStorage.setItem("theme", "dark");
+
+    }
+}
+
+
+// ==========================================
+// LOAD SAVED THEME
+// ==========================================
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const savedTheme = localStorage.getItem("theme");
+
+    const button = document.getElementById("themeToggle");
+
+
+    if (savedTheme === "light") {
+
+        document.body.classList.add("light-mode");
+
+        if (button) {
+            button.textContent = "🌙 Dark Mode";
+        }
+
+    } else {
+
+        if (button) {
+            button.textContent = "☀️ Light Mode";
+        }
+
+    }
+
+});
